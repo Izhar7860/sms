c820bdefd25f3b25e5bde375f466b3e8c6466275
Izhar7860 <khanizharulhaq1111@gmail.com>
2026-05-06 13:24:29 +0530

    git commit

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -3,7 +3,7 @@ window.SMS_FIREBASE_CONFIG = {
   apiKey: "AIzaSyBQZmC0sItp2G9kg9bZNptfatYq2cegJTQ",
   authDomain: "smss-f6c2f.firebaseapp.com",
   projectId: "smss-f6c2f",
-  storageBucket: "smss-f6c2f.firebasestorage.app",
+  storageBucket: "https://console.firebase.google.com/project/smss-f6c2f/database/smss-f6c2f-default-rtdb/data",
   messagingSenderId: "591396592750",
   appId: "1:591396592750:web:3223882a8fda9a85075a7f",
   measurementId: "G-B3W5Y195CS"
